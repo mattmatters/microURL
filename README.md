4f27320ecfe165528e54187ea1f2de5cf93893ab
Matt Lewis <domattthings@gmail.com>
2017-02-18 20:10:12 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Time Stamp Microservice
+
+This is a tiny micro-service that returns the time it received a request.
---
+++ src/app.js
@@ -0,0 +1,106 @@
+import express from 'express';
+import { MongoClient } from 'mongodb';
+
+const publicURL = 'http://poop.com/';
+const mongoURL = 'mongodb://localhost:27017/microURL';
+const LOCAL_PORT = 3000;
+const app = express();
+
+const findUrl = (db, url, cb) => (
+  db.collection('urls').find({ index: parseInt(url, 10) }).toArray()
+    .then((item) => {
+      cb(item[0]);
+    })
+);
+
+const countEntries = (db, cb) => {
+  db.collection('urls').find().count()
+    .then((count) => {
+      cb({ count });
+    });
+};
+
+const findEntries = (db, cb) => {
+  db.collection('urls').find().toArray()
+    .then((items) => {
+      cb(items);
+    });
+};
+
+const insertUrl = (db, url, cb) => {
+  countEntries(db, (response) => {
+    db.collection('urls').insertMany([{
+      number: response.count + 1,
+      originalUrl: url,
+    }], (err, result) => {
+      cb(result);
+    });
+  });
+};
+
+app.get('/', (req, res) => {
+  res.send('MicroUrl Service');
+});
+
+app.get('/url/:tinyUrl', (req, res) => {
+  MongoClient.connect(mongoURL, (err, db) => {
+    if (err === null) {
+      findUrl(db, req.params.tinyUrl, (url) => {
+        if (url) {
+          res.redirect(`http://${url.originalUrl}`);
+        } else {
+          res.send('No url on database');
+        }
+        db.close();
+      });
+    } else {
+      res.send(err);
+      db.close();
+    }
+  });
+});
+
+app.get('/new/:url', (req, res) => {
+  MongoClient.connect(mongoURL, (err, db) => {
+    if (err === null) {
+      insertUrl(db, req.params.url, (result) => {
+        const { number, originalUrl } = result.ops[0];
+        res.send({
+          shortendUrl: publicURL.concat(number),
+          originalUrl,
+        });
+        db.close();
+      });
+    } else {
+      res.send(err);
+      db.close();
+    }
+  });
+});
+
+app.get('/size', (req, res) => {
+  MongoClient.connect(mongoURL, (err, db) => {
+    if (err === null) {
+      countEntries(db, (result) => {
+        res.send(result);
+      });
+      db.close();
+    } else {
+      res.send(err);
+      db.close();
+    }
+  });
+});
+
+app.get('/all', (req, res) => {
+  MongoClient.connect(mongoURL, (err, db) => {
+    findEntries(db, (entries) => {
+      res.send(entries);
+      db.close();
+    });
+  });
+});
+
+app.listen(LOCAL_PORT, () => {
+  console.log(`app is listening on port ${LOCAL_PORT}`);
+});
