7eab00d8c41f513f48502726a372b512ba41ffeb
Matt Lewis <domattthings@gmail.com>
2018-04-14 11:42:23 -0400

    Rewrite app to not require babel and promisfy Mongo Client

--- src/app.js
+++ src/app.js
@@ -1,16 +1,50 @@
-import express from 'express';
-import { MongoClient } from 'mongodb';
-import path from 'path';
-import cors from 'cors';
-import process from 'process';
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const path = require('path');
+const cors = require('cors');
+const process = require('process');
 
-const publicURL = 'http://kindasmallurl.fun/';
-const mongoURL = process.env.MONGODB_URI
-  ? process.env.MONGODB_URI
-  : 'mongodb://localhost:27017/kindasmallurl';
-const PORT = process.env.PORT ? process.env.PORT : 3000;
+const PUBLIC_URL = process.env.PUBLIC_URL || 'http://kindasmallurl.fun/';
+const MONGO_URL = process.env.MONGODB_URI || 'mongodb://localhost:27017/kindasmallurl';
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 
+// Db Wrapper
+const connectDb = () => {
+  return new Promise((resolve, reject) => {
+    MongoClient.connect(MONGO_URL, (err, db) => {
+      return err ? reject(err) : resolve(db);
+    });
+  });
+};
+
+const findUrl = (db, url) => {
+  const lookupVal = { number: url };
+
+  return new Promise((resolve, reject) => {
+    db.collection('urls')
+      .find({ number: url })
+      .toArray()
+      .then(item => resolve(item[0]))
+      .catch(err => reject(err));
+  });
+};
+
+const insertUrl = (db, url) => {
+  const insertVal = {
+    number: randomString(80),
+    originalUrl: url.replace(/http(s)?\:\/\//, ''),
+  };
+
+  return new Promise((resolve, reject) => {
+    db.collection('urls').insertMany([insertVal], (err, result) => {
+      err ? reject(err) : resolve(result);
+    });
+  });
+};
+
+// Util
 const randomString = (len, bits = 36) => {
   let outStr = '';
   let newStr;
@@ -24,90 +58,37 @@ const randomString = (len, bits = 36) => {
   return outStr;
 };
 
-const findUrl = (db, url, cb) =>
-  db
-    .collection('urls')
-    .find({ number: url })
-    .toArray()
-    .then((item) => {
-      cb(item[0]);
-    });
-
-const findEntries = (db, cb) => {
-  db
-    .collection('urls')
-    .find()
-    .toArray()
-    .then((items) => {
-      cb(items);
-    });
-};
-
-const insertUrl = (db, url, cb) => {
-  db.collection('urls').insertMany([
-    {
-      number: randomString(80),
-      originalUrl: url,
-    },
-  ],
-  (err, result) => {
-    cb(result);
-  });
-};
-
+// For local testing
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../static/index.html'));
 });
 
-app.get('/url/:tinyUrl', (req, res) => {
-  MongoClient.connect(mongoURL, (err, db) => {
-    if (!err) {
-      findUrl(db, req.params.tinyUrl, (url) => {
-        if (url) {
-          res.redirect(`http://${url.originalUrl}`);
-        } else {
-          res.send('No url on database');
-        }
-        db.close();
-      });
-    } else {
-      res.send(err);
-      db.close();
-    }
-  });
+// The real api calls
+// @TODO Change to 404 error for unfound url
+app.get('/url/:tinyUrl', async (req, res) => {
+  const db = await connectDb();
+  const url = await findUrl(db, req.params.tinyUrl);
+
+  url ? res.redirect(`http://${url.originalUrl}`) : res.send('No url on database');
+  db.close();
+
+  return;
 });
 
-app.get('/new/:url', (req, res) => {
-  MongoClient.connect(mongoURL, (err, db) => {
-    if (!err) {
-      insertUrl(db, req.params.url, (result) => {
-        const { number, originalUrl } = result.ops[0];
-        res.send({
-          shortendUrl: `${publicURL}url/${number}`,
-          originalUrl,
-        });
-        db.close();
-      });
-    } else {
-      res.send(err);
-    }
+app.get('/new/:url', async (req, res) => {
+  const db = await connectDb();
+  const result = await insertUrl(db, req.params.url);
+  const { number, originalUrl } = result.ops[0];
+  res.send({
+    shortendUrl: `${PUBLIC_URL}url/${number}`,
+    originalUrl: req.params.url,
   });
+  db.close();
 });
 
+
+// Config
 app.options('*', cors());
-
-app.get('/all', (req, res) => {
-  MongoClient.connect(mongoURL, (err, db) => {
-    if (!err) {
-      findEntries(db, (entries) => {
-        res.send(entries);
-        db.close();
-      });
-    } else {
-      res.send(err);
-    }
-  });
-});
 
 app.listen(PORT, () => {
   console.log(`app is listening on port ${PORT}`);
